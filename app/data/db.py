9e2d47ee6e3cf9a3e70a23a0e57a827e52b0db7b
Nijat Akhundzada <akhundzadanijat@gmail.com>
2026-01-21 11:08:12 +0400

    style: Format and Lint

--- app/data/db.py
+++ app/data/db.py
@@ -1,4 +1,3 @@
-from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,5 @@ from app.main import app
 
 @pytest.fixture
 async def client():
-    async with AsyncClient(
-        transport=ASGITransport(app=app), base_url="http://test"
-    ) as c:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as c:
         yield c
